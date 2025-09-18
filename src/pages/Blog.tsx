060c07ab5c2889d504759014c8d0a5e4c7f360e8
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-09-18 16:03:10 +0200

    feat: ajouter les pages Blog, Contact, Portfolio et Service

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <section>
+      <h1>Blog</h1>
+      <p>Page minimaliste.</p>
+    </section>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
-import Button from '@/components/Button/Button.tsx'
+import { PrimaryButtonLink, SecondaryButtonLink } from "@/components/Buttons/ButtonLink";
+
 export default function Home() {
   return (
     <section>
       <h1>Home</h1>
       <p>Page d’accueil minimaliste.</p>
-      <Button onClick={() => alert('👋')}>Dire bonjour</Button>
+      <div style={{ display: 'flex', flexDirection:'column', marginBottom: '1rem' }}>
+        <PrimaryButtonLink to="/contact">Contact</PrimaryButtonLink>
+        <SecondaryButtonLink to="/apropos">A propos</SecondaryButtonLink>
+      </div>
     </section>
   )
 }
