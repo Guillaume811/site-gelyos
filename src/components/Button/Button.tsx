8136d8ef5f3512e32e2ff05792efcf6699d9e6aa
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-09-18 16:03:42 +0200

    feat: supprimer le composant Button et son style associé

--- src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import type { ButtonHTMLAttributes } from 'react'
-import s from './Button.module.scss'
-
-type Props = ButtonHTMLAttributes<HTMLButtonElement>
-
-export default function Button(props: Props) {
-  const { className, ...rest } = props
-  return <button className={[s.btn, className].filter(Boolean).join(' ')} {...rest} />
-}
