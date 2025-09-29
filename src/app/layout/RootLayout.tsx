dc0c90498ec95d7944238ba087f3c967ee5edfa4
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-09-29 10:46:45 +0200

    feat: afficher le composant CallToAction sauf sur la page de contact

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,16 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation  } from 'react-router-dom'
 import DesktopHeader from '@/components/DesktopHeader/DesktopHeader'
+import CallToAction from '@/components/CallToAction/CallToAction'
 import styles from './RootLayout.module.scss'
 import Footer from '@/components/Footer/Footer'
 
 
 export default function RootLayout() {
+
+  const { pathname } = useLocation()
+  const hideCta = pathname === '/contact'
+
   return (
     <div className={styles.wrapper}>
       <DesktopHeader />
       <main className={styles.main}>
         <Outlet />
       </main>
+      {!hideCta && <CallToAction />}
       <Footer />
     </div>
   )
