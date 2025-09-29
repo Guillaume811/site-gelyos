1de5432eb8f08efc6a0787eb91618763562990d2
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-09-29 10:46:34 +0200

    feat: ajouter le contenu d'appel à l'action pour l'audit gratuit du site

---
+++ src/ressources/textes/ctaContent.ts
@@ -0,0 +1,7 @@
+export const ctaContent = {
+    home: {
+        title: "Votre site est-il vraiment optimisé ?",
+        text: "Bénéficiez d'un **audit gratuit de votre site internet** pour identifier ses points forts et ses axes d'amélioration.\nJe vous fournis une analyse claire et des recommandations personnalisées pour booster votre visibilité en ligne.",
+        button: "Recevoir mon audit offert"
+    }
+}
