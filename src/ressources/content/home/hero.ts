f3c820d69ea270c253b6c1fe66ecb8a8987bd4c5
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-10-02 19:24:39 +0200

    fix: ajustement du texte

--- src/ressources/content/home/hero.ts
+++ src/ressources/content/home/hero.ts
@@ -2,8 +2,8 @@ import heroImage from '@/assets/pictures/hero.png'
 import type { Content } from '@/ressources/content/contentTypes'
 
 export const heroContent: Content = {
-    title: 'Développement **Web\n Sur Mesure**',
-    text: 'Je vous conçois des **sites internet sur mesure** avec un **développement web personnalisé** - **site vitrine**, **e-commerce** ou **application web** - pour servir vos objectifs, reflèter votre marque et convertir vos visiteurs en clients.',
+    title: 'Développement\n**Web Sur Mesure**',
+    text: 'Je vous conçois des sites internet sur mesure avec un développement web personnalisé - site vitrine, e-commerce ou application web - pour servir vos objectifs, reflèter votre marque et convertir vos visiteurs en clients.',
     firstButton: {
         label: 'Parlons de votre projet',
         to: '/contact',
