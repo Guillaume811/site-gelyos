ebb513f3b010920c6f9a9c319ed46bf5d84ff948
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-10-03 16:01:14 +0200

    feat: ajout de la section service a la page home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero/Hero";
+import ServicesPreview from "./ServicesPreview/ServicesPreview";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <ServicesPreview />
     </>
   )
 }
