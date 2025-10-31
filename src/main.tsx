6597febad21379ca7eecee1197c733602b204721
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-10-31 11:48:20 +0100

    feat: ajout des style de swiper

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from '@/app/router'
 import '@/styles/main.scss';
+import "./styles/swiper.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
