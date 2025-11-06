32509ff8bd245afe00d9e90617c345ee067fbbcb
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-11-06 10:57:56 +0100

    feat: ajout de l'importation du composant ProjectPreview dans la page Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Hero from "./Hero/Hero";
 import { SectionProcess } from "./SectionProcess/SectionProcess";
 import ServicesPreview from "./ServicesPreview/ServicesPreview";
 import { divAvantagesContent } from "@/ressources/content/home/divAvantagesContent";
+import ProjectPreview from "./ProjectPreview/ProjectPreview";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <ServicesPreview />
       <SectionProcess process={divProcessContent} avantages={divAvantagesContent} />
+      <ProjectPreview />
     </>
   )
 }
