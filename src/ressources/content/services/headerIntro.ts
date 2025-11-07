e46b1a8f5a96926de282632eee14749d77a22898
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-11-07 11:04:42 +0100

    fix: mise à jour du style des éléments em et correction de la mise en forme dans le texte d'introduction des services

--- src/ressources/content/services/headerIntro.ts
+++ src/ressources/content/services/headerIntro.ts
@@ -11,6 +11,6 @@ export const headerServices: PageHeaderContent = {
 }
 
 export const introServices: IntroContent = {
-    text: `En tant que **développeur web sur mesure**, GELYOS vous propose des solutions digitales adaptées aux besoins des entreprises, startups et indépendants.
+    text: `En tant que **développeur web sur mesure**, *GELYOS* vous propose des solutions digitales adaptées aux besoins des entreprises, startups et indépendants.
     De la **création de sites internet personnalisés** à la **conception d'applications web**, en passant par **l'optimisation SEO** et la **maintenance**, chaque service est conçu pour garantir performance, sécurité et visibilité.`
 }
