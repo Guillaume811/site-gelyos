6f4354966ec7ca133cd1e9a2dcb4bb2830ac669d
Guillaume Huguet <guillaumehuguet8@gmail.com>
2025-11-11 14:55:15 +0100

    fix: mise à jour du format de l'image d'en-tête du contact en webp

--- src/ressources/content/contact/contactContent.ts
+++ src/ressources/content/contact/contactContent.ts
@@ -1,5 +1,5 @@
 import type { ContactPageContent } from "../contentTypes";
-import pictureHeaderContact from '@/assets/pictures/header-service.png'
+import pictureHeaderContact from '@/assets/pictures/header-service.webp'
 
 export const contactContent: ContactPageContent = {
     header: {
