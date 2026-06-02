2f286bedaf7ff6ff1c2d4af0718292fd296e98c0
Guillaume Huguet <guillaumehuguet8@gmail.com>
2026-06-02 11:20:12 +0200

    refactor : réorganistion du component DivAvantage

--- src/_pages/Home/SectionProcess/DivAvantages/DivAvantages.tsx
+++ src/_pages/Home/SectionProcess/DivAvantages/DivAvantages.tsx
@@ -15,41 +15,45 @@ export function DivAvantages({ data, className }: DivAvantagesProps) {
 
   return (
     <div className={containerClass}>
-      <div className={styles.left}>
-        <Heading level={3} className={styles.subtitle}>
-          {data.subtitle}
-        </Heading>
-        <Heading level={2} className={styles.title}>
-          {data.title}
-        </Heading>
-        <p className={styles.description}>{data.description}</p>
-      </div>
+      <div className={styles.content}>
+        <div className={styles.top}>
+          <div className={styles.left}>
+            <Heading level={3} className={styles.subtitle}>
+              {data.subtitle}
+            </Heading>
+            <Heading level={2} className={styles.title}>
+              {data.title}
+            </Heading>
+            <p className={styles.description}>{data.description}</p>
+          </div>
 
-      <ul className={styles.grid} role="list">
-        {data.cards.map((card: ProcessCardContent) => (
-          <li key={card.id} className={styles.cell}>
-            <article className={styles.card}>
-              <div className={styles.icon}>
-                <img src={getAssetSrc(card.icon.src)} alt={card.icon.alt} loading="lazy" />
-              </div>
-              <div className={styles.content}>
-                <Heading level={3} className={styles.cardTitle}>
-                  {card.title}
-                </Heading>
-                <p className={styles.cardDescription}>{card.description}</p>
-              </div>
-            </article>
-          </li>
-        ))}
-      </ul>
+          <ul className={styles.grid} role="list">
+            {data.cards.map((card: ProcessCardContent) => (
+              <li key={card.id} className={styles.cell}>
+                <article className={styles.card}>
+                  <div className={styles.icon}>
+                    <img src={getAssetSrc(card.icon.src)} alt={card.icon.alt} loading="lazy" />
+                  </div>
+                  <div className={styles.cardContent}>
+                    <Heading level={3} className={styles.cardTitle}>
+                      {card.title}
+                    </Heading>
+                    <p className={styles.cardDescription}>{card.description}</p>
+                  </div>
+                </article>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className={styles.buttonWrap}>
-        <Link
-          href={data.firstButton.to as string}
-          className={`${buttonStyles.btn} ${buttonStyles.primary} ${styles.button}`}
-        >
-          {data.firstButton.label}
-        </Link>
+        <div className={styles.buttonWrap}>
+          <Link
+            href={data.firstButton.to as string}
+            className={`${buttonStyles.btn} ${buttonStyles.primary} ${styles.button}`}
+          >
+            {data.firstButton.label}
+          </Link>
+        </div>
       </div>
     </div>
   );
